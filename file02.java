15a8b857081024c6256c99fc8db2b9dd9a00ded1
Kostik V <koskossert@gmail.com>
2023-12-26 19:21:10 +0700

    Reverse

--- file02.java
+++ file02.java
@@ -1,14 +1,23 @@
-import java.util.Scanner;
+package com.journaldev.javaprograms;
 
-public class Main {
+public class JavaReverseString {
 
-    public static void main(String[] args) {// так включаются программы,
-        // просто запомнить
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число:");
-        int a = sc.nextInt();
-        sc.close();
-        System.out.println("Квадрат числа:" + (a * a));
+    public static void main(String[] args) {
+        System.out.println(reverseString("abc"));
+        System.out.println(reverseString("123!@#098*"));
+    }
 
+    public static String reverseString(String in) {
+        if (in == null)
+            return null;
+        StringBuilder out = new StringBuilder();
+
+        int length = in.length();
+
+        for (int i = length - 1; i >= 0; i--) {
+            out.append(in.charAt(i));
+        }
+
+        return out.toString();
     }
 }

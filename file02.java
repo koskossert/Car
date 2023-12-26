e0165963c197538e4b86489d4c2e43cd8642d28b
Kostik V <koskossert@gmail.com>
2023-12-26 19:19:06 +0700

    Sqrt

--- file02.java
+++ file02.java
@@ -1,9 +1,14 @@
-public class Test {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        int i = (int) (Math.random() * 10 + 1);
+public class Main {
 
-        System.out.println("Случайное число:" + i);
+    public static void main(String[] args) {// так включаются программы,
+        // просто запомнить
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int a = sc.nextInt();
+        sc.close();
+        System.out.println("Квадрат числа:" + (a * a));
 
     }
 }

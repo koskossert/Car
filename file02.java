f45ad566fbfe3bb1cbab310b3f96658ab088f1f9
Kostik V <koskossert@gmail.com>
2023-12-26 19:17:28 +0700

    Random

--- file02.java
+++ file02.java
@@ -1,19 +1,9 @@
-public class FibonacciSeries {
+public class Test {
 
     public static void main(String[] args) {
-        printFibonacciSeries(10);
-    }
+        int i = (int) (Math.random() * 10 + 1);
 
-    public static void printFibonacciSeries(int count) {
-        int a = 0;
-        int b = 1;
-        int c = 1;
-        for (int i = 1; i <= count; i++) {
-            System.out.print(a + ", ");
-            a = b;
-            b = c;
-            c = a + b;
-        }
+        System.out.println("Случайное число:" + i);
 
     }
 }

660e61693edca8da25b5f2c52f25cd40a6135963
Kostik V <koskossert@gmail.com>
2023-12-26 19:23:59 +0700

    chet

--- file02.java
+++ file02.java
@@ -1,23 +1,8 @@
-package com.journaldev.javaprograms;
-
-public class JavaReverseString {
-
-    public static void main(String[] args) {
-        System.out.println(reverseString("abc"));
-        System.out.println(reverseString("123!@#098*"));
-    }
-
-    public static String reverseString(String in) {
-        if (in == null)
-            return null;
-        StringBuilder out = new StringBuilder();
-
-        int length = in.length();
-
-        for (int i = length - 1; i >= 0; i--) {
-            out.append(in.charAt(i));
-        }
-
-        return out.toString();
+class App {
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
+
+App.isEven(4);
+App.isEven(7);
